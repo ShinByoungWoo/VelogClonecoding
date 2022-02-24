c8302719762d469edae7a20edd044ffda01d106e
daonez <cyongchoi@gmail.com>
2022-02-24 19:32:25 +0900

    상세페에지

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -1,21 +1,21 @@
 //function import
-import React from "react";
-import { actionCreators as postActions } from "../redux/modules/post";
-import { actionCreators as commentActions } from "../redux/modules/comment";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+import React from "react"
+import { actionCreators as postActions } from "../redux/modules/post"
+import { actionCreators as commentActions } from "../redux/modules/comment"
+import { useDispatch } from "react-redux"
+import { useSelector } from "react-redux"
 
 //style import
-import { Grid, Text, Image } from "../elements/Index";
+import { Grid, Text, Image } from "../elements/Index"
 
 //page import
-import Header from "../components/Header";
-import CommentWrite from "../components/CommentWrite";
-import CommentList from "../components/CommentList";
+import Header from "../components/Header"
+import CommentWrite from "../components/CommentWrite"
+import CommentList from "../components/CommentList"
 
 const Detail = (props) => {
-  const dispatch = useDispatch();
-  console.log(props);
+  const dispatch = useDispatch()
+  console.log(props)
   // const post_id = props.match.params.postid;
   // React.useEffect(() => {
   //   dispatch(postActions.getOnePostFB(post_id));
@@ -23,23 +23,28 @@ const Detail = (props) => {
   // }, []);
 
   //댓글 불러오기
-  const id = props.match.params.post_id;
+  const id = props.match.params.post_id
 
   React.useEffect(() => {
-    dispatch(postActions.getOnePostDB(id));
-  }, []);
+    dispatch(postActions.getOnePostDB(id))
+  }, [])
 
-  const post_list = useSelector((state) => state.post.detail);
+  const post_list = useSelector((state) => state.post.detail)
 
-  console.log(id);
-  console.log(post_list);
+  console.log(id)
+  console.log(post_list)
 
-  const title = post_list.detail_post.title;
-  const email = post_list.email;
-  const content = post_list.detail_post.content;
-  const createDate = post_list.detail_post.createdAt;
-  const img_url = post_list.detail_post.img_url;
-  const post_id = post_list.detail_post.post_id;
+  const {
+    detail_post: { title, content, img_url, createdAt },
+    email,
+  } = post_list
+
+  // const title = post_list.detail_post.title
+  // const email = post_list.email
+  // const content = post_list.detail_post.content
+  // const createDate = post_list.detail_post.createdAt
+  // const img_url = post_list.detail_post.img_url
+  // const post_id = post_list.detail_post.post_id
 
   return (
     <React.Fragment>
@@ -51,13 +56,9 @@ const Detail = (props) => {
           </Text>
           <Grid is flex>
             <Text margin="5px ">{email}</Text>
-            <Text margin="5px ">{createDate}</Text>
+            <Text margin="5px ">{createdAt}</Text>
           </Grid>
-          <Image
-            src={img_url}
-            size="20"
-            border_radisu="10px"
-          />
+          <Image src={img_url} size="20" border_radisu="10px" />
           <Text size="18px" line_height="1.5">
             {content}
           </Text>
@@ -66,7 +67,7 @@ const Detail = (props) => {
         <CommentList />
       </Grid>
     </React.Fragment>
-  );
-};
+  )
+}
 
-export default Detail;
+export default Detail
